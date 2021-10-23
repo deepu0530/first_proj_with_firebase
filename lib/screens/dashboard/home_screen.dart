4d0613fd4e887d18b6e481bdac44200d49162478
DeepikaLakkimsetti <deepikadeepu1790@gmail.com>
2021-10-23 21:04:22 +0530

    updated

--- lib/screens/dashboard/home_screen.dart
+++ lib/screens/dashboard/home_screen.dart
@@ -1,7 +1,8 @@
+import 'package:first_proj/utils/appcolors.dart';
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatefulWidget {
-  const HomeScreen({ Key? key }) : super(key: key);
+  const HomeScreen({Key? key}) : super(key: key);
 
   @override
   _HomeScreenState createState() => _HomeScreenState();
@@ -10,6 +11,64 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      drawer: Drawer(
+        // Add a ListView to the drawer. This ensures the user can scroll
+        // through the options in the drawer if there isn't enough vertical
+        // space to fit everything.
+        child: ListView(
+          // Important: Remove any padding from the ListView.
+          padding: EdgeInsets.zero,
+          children: [
+            const DrawerHeader(
+              decoration: BoxDecoration(
+                color: Colors.blue,
+              ),
+              child: Text('Drawer Header'),
+            ),
+            ListTile(
+              title: const Text('Item 1'),
+              onTap: () {
+                // Update the state of the app.
+                // ...
+              },
+            ),
+            ListTile(
+              title: const Text('Item 2'),
+              onTap: () {
+                // Update the state of the app.
+                // ...
+              },
+            ),
+          ],
+        ),
+      ),
+      appBar: AppBar(
+        backgroundColor: AppColors.appColor,
+        centerTitle: true,
+        title: Text(
+          "Flutter",
+          style: TextStyle(
+              fontSize: 20, fontWeight: FontWeight.w700, color: Colors.white),
+        ),
+        actions: [
+          Padding(
+            padding: const EdgeInsets.all(5),
+            child: Icon(
+              Icons.person,
+              color: AppColors.whiteColor,
+              size: 30,
+            ),
+          )
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          // Add your onPressed code here!
+        },
+        child: Center(child: const Icon(Icons.logout)),
+        backgroundColor: AppColors.appColor,
+      ),
+    );
   }
 }
